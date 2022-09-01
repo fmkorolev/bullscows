d39bf50a120e6f73195d5cf44b1b8daecaa2fdcb
filip korolev <filippok089@gmail.com>
2022-09-01 15:58:26 +0300

    dz is done

---
+++ main.py
@@ -0,0 +1,89 @@
+from telegram.ext import Updater, CommandHandler, Filters, MessageHandler, ConversationHandler,updater
+import os
+from game import *
+
+TOKEN = "5782848176:AAEk7enJWVn5SLH6knQE0MY-tgAn-d0R7_w"
+
+token = os.getenv(TOKEN)
+updater = Updater(TOKEN)
+dispatcher = updater.dispatcher
+
+guess = None
+PLAYER, MOVE = 0, 1
+player_name = None
+
+print('Бот работает...')
+
+
+def start(update, context):
+    global num, count
+    num = generate_num()  # "загадываем" число
+    count = 1
+    print(num)  # для тестирования выводим число в консоль
+    context.bot.send_message(update.effective_chat.id,
+                             f'Привет! Сыграем в "Быки и Коровы"?\n'
+                             f'Правила просты и доступны по <a href="https://bit.ly/3bVRJMr">ссылке</a>',
+                             parse_mode='HTML', disable_web_page_preview=True)
+    context.bot.send_message(update.effective_chat.id, 'Меня зовут БОТ БЫК, а тебя?')
+    return PLAYER
+
+
+def get_name(update, context):
+    global player_name, count
+    player_name = update.message.text
+    context.bot.send_message(update.effective_chat.id, 'Игра началась... <b>У тебя есть 10 попыток...</b>',
+                             parse_mode='HTML')
+    context.bot.send_message(update.effective_chat.id, 'Введи число...')
+    context.bot.send_message(update.effective_chat.id, f'Попытка {count}...')
+    return MOVE
+
+
+def take_move(update, context):
+    global guess, count
+    try:
+        guess = int(update.message.text)
+    except:
+        context.bot.send_message(update.effective_chat.id, f'Введи четырехзначное число...')
+        return MOVE
+    if not check_duplicates(guess):
+        context.bot.send_message(update.effective_chat.id, f'В загаданном числе цифры не повторяются')
+        return MOVE
+    if guess < 1000 or guess > 9999:
+        context.bot.send_message(update.effective_chat.id, f'Загаданное число состоит из 4 цифр')
+        return MOVE
+    count += 1  
+    bull_cow = bulls_and_cows(num, guess)
+    if bull_cow[0] == 4:  
+        context.bot.send_message(update.effective_chat.id,
+                                 f'Браво, {player_name}! Число {num} отгадано за {count - 1} попыток')
+        return ConversationHandler.END
+    if count <= 10:  
+        context.bot.send_message(update.effective_chat.id, f'быков - {bull_cow[0]} | коров - {bull_cow[1]}')
+        context.bot.send_message(update.effective_chat.id, f'Попытка {count}...')
+        return MOVE
+    else:  
+        context.bot.send_message(update.effective_chat.id,
+                                 f'Увы, {player_name}, ты потратил свои {count - 1} попыток и проиграл...')
+        context.bot.send_message(update.effective_chat.id,
+                                 f'В интернете есть описание нескольких выигрышных стратегий для этой игры =)\n'
+                                 f'Советую почитать! Жду тебя снова =)')
+        return ConversationHandler.END
+
+
+def exit(update, context):
+    context.bot.send_message(update.effective_chat.id, 'Game over')
+    return ConversationHandler.END
+
+
+conv_handler = ConversationHandler(
+    entry_points=[CommandHandler('start', start)],
+    states={
+        PLAYER: [MessageHandler(Filters.text, get_name)],
+        MOVE: [MessageHandler(Filters.text, take_move)]
+    },
+    fallbacks=[CommandHandler('exit', exit)])
+
+dispatcher.add_handler(conv_handler)
+
+updater.start_polling()
+updater.idle()
