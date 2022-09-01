459985105a67c080a81b2abd01ae7d58cff299de
filip korolev <filippok089@gmail.com>
2022-09-01 15:58:53 +0300

    done

---
+++ game.py
@@ -0,0 +1,31 @@
+import random
+
+
+def check_duplicates(num: int) -> bool:
+    num_li = get_digits(num)
+    return len(num_li) == len(set(num_li))
+
+
+def get_digits(num: int) -> list:
+    return [int(i) for i in str(num)]
+
+
+def generate_num() -> int:
+    while True:
+        num = random.randint(1000, 9999)
+        if check_duplicates(num):
+            return num
+
+
+
+def bulls_and_cows(num: int, guess: int) -> list[int]:
+    bull_cow = [0, 0]
+    num_list = get_digits(num)
+    guess_list = get_digits(guess)
+    for i, j in zip(num_list, guess_list):
+        if j in num_list:  # есть ли цифра в списке
+            if j == i:  # цифра есть и стоит на своём месте
+                bull_cow[0] += 1
+            else:  # цифра есть, но стоит не на своём месте
+                bull_cow[1] += 1
+    return bull_cow
